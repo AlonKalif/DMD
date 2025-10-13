57741b1d41924f2d1d6e7e01d8cde7e3b27474a6
alon.kalif <alonkalif15@gmail.com>
2025-10-13 15:38:14 +0300

    change default player window image to animation

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,4 @@
 // File: /src/config.ts
 export const API_BASE_URL = 'http://localhost:8080';
+// export const DEFAULT_PLAYER_WINDOW_IMG = '/Red_Dragon_5eR.webp'
+export const DEFAULT_PLAYER_WINDOW_IMG = 'https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3bjczZjJsbjY2ZnpxNTRmcTAzOG9iNnM3M216aXpyNWszaTR5eWFoMiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/iBlgTxSS20NLdCxvDW/giphy.gif'
--- frontend/src/pages/PlayerDisplayPage.tsx
+++ frontend/src/pages/PlayerDisplayPage.tsx
@@ -1,14 +1,11 @@
 // File: /src/pages/PlayerDisplayPage.tsx
-import {
-    useBroadcastChannel,
-    BroadcastMessage,
-} from '../hooks/useBroadcastChannel';
+import { useBroadcastChannel, BroadcastMessage} from '../hooks/useBroadcastChannel';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { setCurrentContent, clearContent } from 'features/display/displaySlice';
+import { DEFAULT_PLAYER_WINDOW_IMG } from 'config';
 
 export default function PlayerDisplayPage() {
     // Get the dispatch function and select the state from the Redux store.
-    const defaultPlayerImage = '/images/Red_Dragon_5eR.webp';
     const dispatch = useAppDispatch();
     const currentContent = useAppSelector((state) => state.display.currentContent);
 
@@ -27,19 +24,23 @@ export default function PlayerDisplayPage() {
     return (
         <div className="flex h-screen w-screen flex-col items-center justify-center bg-black p-4 text-white">
             {!currentContent && (
-                // <h1 className="text-4xl font-bold">Player Window</h1>
-              <img src={defaultPlayerImage}/>
+                <figure>
+                    <img
+                        src={DEFAULT_PLAYER_WINDOW_IMG}
+                        alt='nat 1'
+                        // className="flex h-screen w-screen flex-col items-center justify-center "
+                        className="max-h-[80vh] w-auto rounded-lg object-contain"
+                    />
+                </figure>
             )}
             {currentContent?.type === 'image' && (
                 <figure>
                     <img
                         src={currentContent.payload.url}
                         alt={currentContent.payload.caption}
+                        // className="flex h-screen w-screen flex-col items-center justify-center "
                         className="max-h-[80vh] w-auto rounded-lg object-contain"
                     />
-                    <figcaption className="mt-4 text-center text-2xl italic">
-                        {currentContent.payload.caption}
-                    </figcaption>
                 </figure>
             )}
         </div>
